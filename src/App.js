bb6c93e4fe8a7f49fc06f6c056eebd9e0475dc36
viktorferenczi <v.ferenczi@falatozz.hu>
2021-11-10 16:26:27 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+/**
+ * 
+ * 
+ *  Created at: 2021-11-10
+ *  Creator: Viktor Ferenczi
+ *  Copyright https://github.com/viktorferenczi
+ * 
+ * 
+ */
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
