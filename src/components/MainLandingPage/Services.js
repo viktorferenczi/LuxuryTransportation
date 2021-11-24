01bcb563eab5014ab9f4d68209a389323ddf8683
viktorferenczi <v.ferenczi@falatozz.hu>
2021-11-24 21:32:40 +0100

    services init

---
+++ src/components/MainLandingPage/Services.js
@@ -0,0 +1,44 @@
+import '../../App.css';
+
+export const Services = () => {
+    return(
+        <div>
+            <h1>SERVICES</h1>
+            <p>...</p>
+
+            <h4>PARTIES</h4>
+            <p>   
+                The prerequisite of a wonderful experience is a good looking car with a well equipped luxury interior.
+                The best business events starts with 5 Star Luxury Transportation!
+            </p>
+
+            <h4>AIRPORT TRANSPORTATION</h4>
+            <p>   
+            If you have to arrive in time, or you just want to surprise your relatives with a luxury car in the airport we offer you a good choice!
+            </p>
+
+            <h4>WEDDINGS</h4>
+            <p>   
+            The best moment in your life is your wedding, so I guess you want a perfect vehicle for yourself. The answer is 5 Star Luxury  Transportaton!  Courteous driver? Outstanding luxury? No probelm for us!
+            </p>
+
+            <h4>CASINO, BUSINESS</h4>
+            <p>   
+            You have to pay attention for your business event, casino meeting which includes a luxury car. The first impression is important! Do you want a preferable appearance? Then you need our vehicles!
+            </p>
+
+            <h4>BIRTHDAYS</h4>
+            <p>   
+            Surprise your friend's birthday or you can actually surprise even yourself with a luxury car. You can find the best car here even if the party has a lot of members! 
+            </p>
+
+            <h4>PROMOTIONS, NEOGITATIONS</h4>
+            <p>   
+            A good bachelorette party must have a luxury car. Drinks, good music, huge party, your best friends. Warning: Great memories, do not miss it!
+            </p>
+
+            <h2>WE SPECIALIZE IN MIAMI TRANSPORTATION SERVICE TO ORLANDO AND KEYS FROM FORT LAUDERDALE OR MIAMI AIRPORT</h2>
+
+        </div>
+    );
+}
