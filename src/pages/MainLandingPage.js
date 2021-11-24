d3501c39f93c641fb4a158fc7e7237999a155376
viktorferenczi <v.ferenczi@falatozz.hu>
2021-11-24 21:34:31 +0100

    freshly implemented componens import

--- src/pages/MainLandingPage.js
+++ src/pages/MainLandingPage.js
@@ -1,8 +1,20 @@
 
+import { Footer } from "../components/Footer/Footer";
 import {Header} from "../components/Header/Header";
+import { CarChooser } from "../components/MainLandingPage/CarChooser";
+import { FounderInfo } from "../components/MainLandingPage/FounderInfo";
+import { Promotion } from "../components/MainLandingPage/Promotion";
+import { Services } from "../components/MainLandingPage/Services";
 
 export const MainLandingPage = () => {
     return(
-        <Header/>
+        <>
+            <Header/>
+            <FounderInfo/>
+            <CarChooser/>
+            <Promotion/>
+            <Services/>
+            <Footer/>
+        </>
     )
 }
