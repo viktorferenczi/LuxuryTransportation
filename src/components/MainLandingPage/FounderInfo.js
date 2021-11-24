9eb2a2939750f45af975cf3549643fd77b0c2d1d
viktorferenczi <v.ferenczi@falatozz.hu>
2021-11-24 21:32:25 +0100

    founderinfo init

---
+++ src/components/MainLandingPage/FounderInfo.js
@@ -0,0 +1,26 @@
+import '../../App.css';
+
+export const FounderInfo = () => {
+    return(
+        <div>
+           <h1>Welcome</h1>
+           <h4>Welcome to our page, I am Zsolt Molnar, the founder of
+MIAMI TRANSPORTATION.</h4>
+            <p>We have been in this business for years, and we developed a way to satisfy all your needs in miami transportation. 
+From the start we used Chevrolet SUVs and later we expanded our collection with Mercedes Sprinters, Lincoln MKZs, and more. 
+You can take a look at our vehicles online anytime. If you want to see the car in person, call us, and we will be at your service to preview the vehicle before booking.</p>
+  <h4>Why Miami Suv Transportation?</h4>
+  <ul>
+      <li>With several years of professional experience</li>
+      <li>Reliable, continuously maintained cars</li>
+      <li>Vehicles for special needs: weddings, partys, birthdays, business events</li>
+      <li>Professional drivers, with demanding appearance</li>
+      <li>Fully licensed and insured vehicles</li>
+  </ul>
+  <p>Choose your favorite vehicle, and one of our services, and we're going to provide an unforgettable ride!</p>
+  <p>Travel with us, we are looking forward for your call!</p>
+  <p>Best Regards:</p>
+  <p>Zsolt Molnar</p>
+    </div>
+    );
+}
