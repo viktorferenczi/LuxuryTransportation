668480f4074284c7ab9308b6cc031195b4fad6d4
viktorferenczi <v.ferenczi@falatozz.hu>
2021-11-25 19:06:35 +0100

    booking page implemented

---
+++ src/pages/BookingPage.js
@@ -0,0 +1,10 @@
+import { Header } from "../components/Header/Header"
+
+export const BookingPage = () => {
+    return(
+        <div>
+            <Header viewing={"Main"}/>
+            <p>booking</p>    
+        </div>
+    )
+}
