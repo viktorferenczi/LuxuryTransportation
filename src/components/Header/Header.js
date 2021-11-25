6a5ea9162aa8260b5ebea37773de3056ee650450
feher.attila96@gmail.com <feher.attila96@gmail.com>
2021-11-25 22:50:43 +0100

    Header css modifications and header modifications

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,26 +4,31 @@ import '../../App.css';
 export const Header = () => {
     return(
         <div>
-            <header className="header" >
-                <img className="logo-image" src={logo} alt="Logo"/>
-                <h3 className="header-h3" >Luxury Transportation</h3>
-                <div className="header-contact">
-                    <p className="contact">fivestar-inc@hotmail.com</p>
+            <div className="header" >
+               
+                <div className="main-menu">
+                    <p className="first-menu-item">Home</p>
+                    <p className="main-menu-item">Events</p>
+                    <p className="main-menu-item">Prices</p>
+                    <p className="main-menu-item">Booking</p>
+                    <p className="main-menu-item">Contact</p>
+                    <div className="phone-number-contact">
+                        <p className="contact">(305) 606-0139</p>
+                    </div>
+                </div>
+               
+            </div>
+            <div className="main-menu">
+            </div>
+        </div>
+    );
+
+     /*<p className="contact">fivestar-inc@hotmail.com</p>
+      *  <h3 className="header-h3" >Luxury Transportation</h3>
                     <p className="contact">(305) 606-0139</p>
                     <div className="social-div">
                         <p className="contact">fb</p>
                         <p className="contact">google</p>
                         <p className="contact">googlemaps</p>
-                    </div>
-                </div>
-            </header>
-            <div className="main-menu">
-                <p className="first-menu-item">Home</p>
-                <p className="main-menu-item">Events</p>
-                <p className="main-menu-item">Prices</p>
-                <p className="main-menu-item">Booking</p>
-                <p className="main-menu-item">Contact</p>
-            </div>
-        </div>
-    );
+                    </div>*/
 }
