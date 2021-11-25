d16082e0fee02717440becb71f4979517cd0ae1e
viktorferenczi <v.ferenczi@falatozz.hu>
2021-11-25 22:15:47 +0100

    contactpage init

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,10 +1,25 @@
-import { Header } from "../components/Header/Header"
+import { Header } from "../components/Header/Header";
+import { Footer } from "../components/Footer/Footer";
+import { CompanyInfo } from "../components/ContactPage/CompanyInfo";
+import { RidePlanner } from "../components/ContactPage/RidePlanner";
+import { EmailSender } from "../components/ContactPage/EmailSender";
+
 
 export const ContactPage = () => {
     return(
         <div>
             <Header viewing={"Main"}/>
-            <p>contact</p>    
+            <h3>Contact Information</h3>
+            
+            <div style={{flexDirection:"row", display:'flex'}}>
+                <CompanyInfo/>
+                <RidePlanner/>
+                <EmailSender/>
+            </div>
+          
+            <Footer/>
         </div>
     )
 }
+
+
