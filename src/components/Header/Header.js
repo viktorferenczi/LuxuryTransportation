c85e02f24be78baa18f09bc5b583c2c75c790eec
viktorferenczi <v.ferenczi@falatozz.hu>
2021-11-25 19:06:18 +0100

    changing p tag to a tag

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import logo from '../../logo.svg';
 import '../../App.css';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
     return(
@@ -18,11 +19,11 @@ export const Header = () => {
                 </div>
             </header>
             <div className="main-menu">
-                <p className="first-menu-item">Home</p>
-                <p className="main-menu-item">Events</p>
-                <p className="main-menu-item">Prices</p>
-                <p className="main-menu-item">Booking</p>
-                <p className="main-menu-item">Contact</p>
+                <a href={"/"} className="first-menu-item">Home</a>
+                {/*<a className="first-menu-item">Events</a>*/}
+                <a href={"/prices"} className="main-menu-item">Prices</a>
+                <a href={"/booking"} className="main-menu-item">Booking</a>
+                <a href={"/contact"} className="main-menu-item">Contact</a>
             </div>
         </div>
     );
