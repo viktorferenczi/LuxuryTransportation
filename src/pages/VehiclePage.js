0cd821dd87c34a9094548d27def2094beb4a851b
viktorferenczi <v.ferenczi@falatozz.hu>
2021-11-29 20:32:35 +0100

    header added to vehicle page

--- src/pages/VehiclePage.js
+++ src/pages/VehiclePage.js
@@ -1,8 +1,10 @@
+import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer/Footer"
 
 export const VehiclePage = () => {
     return(
         <div>
+            <Header/>
             <Footer/>
         </div>
     )
