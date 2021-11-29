6c02237b29317f67efbd20ad0c33edc216ffb459
feher.attila96@gmail.com <feher.attila96@gmail.com>
2021-11-30 00:13:51 +0100

    form the services div and made for this div some css class and add to the files an image, which i used for the test, now we just need to download the proper pictures for the other service theme and then just use them

--- src/components/MainLandingPage/Services.js
+++ src/components/MainLandingPage/Services.js
@@ -1,44 +1,79 @@
 import '../../App.css';
+import servicePartiesIcon from "../../store/img/servicePartiesIcon.webp";
 
 export const Services = () => {
     return(
         <div>
-            <h1>SERVICES</h1>
+            <h1 style={{color: '#b1955a' }} >SERVICES</h1>
             <p>...</p>
-
-            <h4>PARTIES</h4>
-            <p>   
-                The prerequisite of a wonderful experience is a good looking car with a well equipped luxury interior.
-                The best business events starts with 5 Star Luxury Transportation!
-            </p>
-
-            <h4>AIRPORT TRANSPORTATION</h4>
-            <p>   
-            If you have to arrive in time, or you just want to surprise your relatives with a luxury car in the airport we offer you a good choice!
-            </p>
-
-            <h4>WEDDINGS</h4>
-            <p>   
-            The best moment in your life is your wedding, so I guess you want a perfect vehicle for yourself. The answer is 5 Star Luxury  Transportaton!  Courteous driver? Outstanding luxury? No probelm for us!
-            </p>
-
-            <h4>CASINO, BUSINESS</h4>
-            <p>   
-            You have to pay attention for your business event, casino meeting which includes a luxury car. The first impression is important! Do you want a preferable appearance? Then you need our vehicles!
-            </p>
-
-            <h4>BIRTHDAYS</h4>
-            <p>   
-            Surprise your friend's birthday or you can actually surprise even yourself with a luxury car. You can find the best car here even if the party has a lot of members! 
-            </p>
-
-            <h4>PROMOTIONS, NEOGITATIONS</h4>
-            <p>   
-            A good bachelorette party must have a luxury car. Drinks, good music, huge party, your best friends. Warning: Great memories, do not miss it!
-            </p>
-
-            <h2>WE SPECIALIZE IN MIAMI TRANSPORTATION SERVICE TO ORLANDO AND KEYS FROM FORT LAUDERDALE OR MIAMI AIRPORT</h2>
-
+            <div>
+                <div className="service-main-div">
+                    <div className="service-inner-div">
+                        <img className="service-icon-image" src={servicePartiesIcon} /> 
+                        <h5 className="service-name" >PARTIES</h5>
+                        <p style={{ marginTop: '2rem' }}>   
+                            The prerequisite of a wonderful experience is a good looking car with a well equipped luxury interior.
+                            The best business events starts with 5 Star Luxury Transportation!
+                        </p>
+                    </div>
+                </div>
+                <div className="service-main-div">
+                   
+                    <div className="service-inner-div">
+                        <img className="service-icon-image" src={servicePartiesIcon} /> 
+                        <h5 className="service-name">AIRPORT TRANSPORTATION</h5>
+                        <p >    
+                             If you have to arrive in time, or you just want to surprise your relatives with a luxury car in the airport we offer you a good choice!
+                        </p>
+                    </div>
+                </div>
+                <div className="service-main-div">
+                    
+                    <div className="service-inner-div">
+                        <img className="service-icon-image" src={servicePartiesIcon} /> 
+                        <h5 className="service-name">WEDDINGS</h5>
+                        
+                        <p style={{ marginTop: '2rem' }}>   
+                            The best moment in your life is your wedding, so I guess you want a perfect vehicle for yourself. The answer is 5 Star Luxury  Transportaton!  Courteous driver? Outstanding luxury? No probelm for us!
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="service-main-div">
+                   
+                    <div className="service-inner-div">
+                        <img className="service-icon-image" src={servicePartiesIcon} /> 
+                        <h5 className="service-name">CASINO, BUSINESS</h5>
+                        <p style={{ marginTop: '2rem' }}>   
+                             You have to pay attention for your business event, casino meeting which includes a luxury car. The first impression is important! Do you want a preferable appearance? Then you need our vehicles!
+                        </p>
+                    </div>
+                </div>
+                <div className="service-main-div">
+                    
+                    <div className="service-inner-div">
+                        <img className="service-icon-image" src={servicePartiesIcon} /> 
+                        <h5 className="service-name">BIRTHDAYS</h5>
+                        <p style={{ marginTop: '2rem' }}>   
+                            Surprise your friend's birthday or you can actually surprise even yourself with a luxury car. You can find the best car here even if the party has a lot of members! 
+                        </p>
+                    </div>
+                </div>
+                <div className="service-main-div">
+                   
+                    <div className="service-inner-div">
+                        <img className="service-icon-image" src={servicePartiesIcon} /> 
+                        <h5 className="service-name">PROMOTIONS, NEOGITATIONS</h5>
+                        <p >   
+                             A good bachelorette party must have a luxury car. Drinks, good music, huge party, your best friends. Warning: Great memories, do not miss it!
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div style={{ display: 'inline-block' }}>
+                <h2>WE SPECIALIZE IN MIAMI TRANSPORTATION SERVICE TO ORLANDO AND KEYS FROM FORT LAUDERDALE OR MIAMI AIRPORT</h2>
+            </div>
         </div>
     );
 }
