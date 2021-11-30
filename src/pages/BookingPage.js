0ce3f373cef0b7366c9213c51556f04f96fb70d5
viktorferenczi <v.ferenczi@falatozz.hu>
2021-11-30 22:19:10 +0100

    service chooser added to bookingpage

--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -1,11 +1,13 @@
 import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer/Footer";
+import { ServiceChooser } from "../components/BookingPage/ServiceChooser";
  
 export const BookingPage = () => {
     return(
-        <div>
+        <div style={{marginTop:"8rem"}}>
             <Header/>
             <p>booking</p>
+            <ServiceChooser/>
             <Footer/>
         </div>
     )
