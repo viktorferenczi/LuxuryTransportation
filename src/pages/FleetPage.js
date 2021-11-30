82b53f9049c238efedc105b87f62c5e2f43e4e0c
viktorferenczi <v.ferenczi@falatozz.hu>
2021-11-30 21:33:49 +0100

    fleet page refact

--- src/pages/FleetPage.js
+++ src/pages/FleetPage.js
@@ -1,47 +1,46 @@
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
+import { Link } from "react-router-dom";
 
 import chevrolet from './../store/img/fleet/chevrolet/chevrolet_small.png';
 import lincoln from './../store/img/fleet/lincoln/lincoln_small.png';
 import mercedes_basic from './../store/img/fleet/mercedes_basic/mercedes_small.png';
 import mercedes_premium from './../store/img/fleet/mercedes_premium/mercedes_small.png';
 
+import cars from '../fleet.json';
+
 export const FleetPage = () => {
 
-    let carList = [
-        {
-            name:'CHEVROLET SUBURBAN',
-            year: '2020',
-            seats: '6 seats',
-            imgPath: chevrolet,
-        },
-        {
-            name:'MERCEDES SPRINTER',
-            year: '2020',
-            seats: '6 seats',
-            imgPath:mercedes_basic
-        },
-        {
-            name:'MERCEDES SPRINTER EXECUTIVE',
-            year: '2020',
-            seats: '13 seats',
-            imgPath:mercedes_basic
-        },
-        {
-            name:'LINCOLN MKZ SEDAN',
-            year: '2020',
-            seats: '6 seats',
-            imgPath: lincoln
-        },
-    ]
+    let carList = Object.values(cars);
 
+    carList.forEach(car => {
+        switch (car.imgPath) {
+            case "chevrolet":
+                car.imgPath = chevrolet;
+                break;
+            case "lincoln":
+                car.imgPath = lincoln;
+                break;
+            case "mercedes_basic":
+                car.imgPath = mercedes_basic;
+                break;
+            case "mercedes_premium":
+                car.imgPath = mercedes_premium;
+                break;
+            default:
+                break;
+        }
+    });
     
     const listItems = carList.map( (car) =>
         <div key={car.name}>
             <img alt={car.name} src={car.imgPath}/>
             <p>{car.name}</p>
             <p>{car.year} - {car.seats}</p>
-            <a href="/vehicle">More Info</a>
+            <Link
+                to={`/vehicle?id=${car.id}`}
+                state={{ car }}
+            >More Info</Link>
         </div>
     );
 
@@ -49,9 +48,9 @@ export const FleetPage = () => {
     return (
         <div>
             <Header/>
-            <div style={{display:'flex',flexDirection:'row', marginTop:"8rem"}}>
-                {listItems}
-            </div>
+                <div style={{display:'flex',flexDirection:'row', marginTop:"8rem"}}>
+                    {listItems}
+                </div>
             <Footer/>
         </div>
     );
