a591ec4cb5ee5676c5bab906e61b443df20eb8c1
viktorferenczi <v.ferenczi@falatozz.hu>
2021-11-30 21:34:03 +0100

    vehicle page implemented

--- src/pages/VehiclePage.js
+++ src/pages/VehiclePage.js
@@ -1,10 +1,29 @@
 import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer/Footer"
+import { useLocation } from "react-router";
+
+import fleet from '../fleet.json';
 
 export const VehiclePage = () => {
+    const location = useLocation()
+    const carId = location.state.car.id;
+    let carList = Object.values(fleet);
+    let displayedCar = null;
+
+    carList.forEach(car => {
+        if(car.id == carId){
+            displayedCar = car;
+        }
+    });
+
     return(
-        <div>
+        <div style={{marginTop:'9rem'}}>
             <Header/>
+            <img src={displayedCar.imgPath}/>
+            <p>{displayedCar.name}</p>
+            <p>{displayedCar.year}</p>
+            <p>{displayedCar.seats}</p>
+            <p>{displayedCar.carInfo}</p>
             <Footer/>
         </div>
     )
