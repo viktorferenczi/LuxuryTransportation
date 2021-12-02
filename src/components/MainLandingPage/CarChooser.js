2f129e077106c98a7903a81086c01af6bcc9bbdb
feher.attila96@gmail.com <feher.attila96@gmail.com>
2021-12-03 00:41:43 +0100

    set the z-index to -1 in the CarChooser.js to prevent the header go behind theese pictures

--- src/components/MainLandingPage/CarChooser.js
+++ src/components/MainLandingPage/CarChooser.js
@@ -6,15 +6,15 @@ import mercedes from "../../store/img/fleet/mercedes_basic/mercedes_small.png";
 export const CarChooser = () => {
 
     return (
-        <div style={{ display: "inline-grid",  height: '34rem', width:'90rem', marginBottom: '10rem', marginTop: '2rem'}}>
+        <div style={{ display: "inline-grid", height: '34rem', width: '90rem', marginBottom: '10rem', marginTop: '2rem', zIndex: '-1' }}>
             <div className='background-image'>
                 <h1>CHOOSE FROM OUR VEHICLES</h1>
                 
             </div>
-            <div style={{ position: "absolute", top: '97%', justifySelf: 'end' }}>
+            <div style={{ position: "absolute", top: '97%', justifySelf: 'end', zIndex: '-1' }}>
                 <img style={{ width: "35rem", marginRight: '5rem' }} src={mercedes}></img>
             </div>
-            <div style={{ position: "absolute", top: '97%' }}>
+            <div style={{ position: "absolute", top: '97%', zIndex: '-1' }}>
                 <img style={{ width: "35rem", marginLeft: '5rem' }} src={chevrolet}></img>
             </div>
            
